f51c5fa5a800c5d28d2941916a04f04d32058a70
NNyn0 <64975382+nionon@users.noreply.github.com>
2026-03-25 19:24:37 +0200

    Refactor Motors class to Motor with new methods
    Updated Motors class to Motor class and modified methods.

--- flightcode/motors.cpp
+++ flightcode/motors.cpp
@@ -1,22 +1,17 @@
-#include "motors.h"
+#include "motor.h"
 #include <Tonttulib.h>
 
 static Tonttulib tLib;
 
-void Motors::init() {
+void Motor::init() {
     tLib.init();
 }
 
-void Motors::setAll(uint16_t us) {
-    tLib.motors.set(1, us);
-    tLib.motors.set(2, us);
-    tLib.motors.set(3, us);
-    tLib.motors.set(4, us);
+void Motor::setAll(uint16_t us) {
+    Serial.println(".");
 }
 
-void Motors::set(uint16_t m1, uint16_t m2, uint16_t m3, uint16_t m4) {
-    tLib.motors.set(1, m1);
-    tLib.motors.set(2, m2);
-    tLib.motors.set(3, m3);
-    tLib.motors.set(4, m4);
+void Motor::set(uint16_t m1, uint16_t m2, uint16_t m3, uint16_t m4) {
+        Serial.println(".");
+
 }
